51fbca13270b605384698e10842390c94905c51a
en6ineer <d.kotenko@railsystems.kz>
2024-01-29 08:07:32 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+let number = 10
+let firstName = 'Maxim'
+number = 15
+const request = true
+let result = document.getElementById('result')
+console.log(result.textContent)
+result.textContent = 12345
