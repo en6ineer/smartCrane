e599e5b6aaa90ed20b2732af6790d830bf562296
en6ineer <D.Kotenko@RailSystems.kz>
2024-01-30 15:40:24 +0600

    succes!

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
-let number = 10
-let firstName = 'Maxim'
-number = 15
-const request = true
-let result = document.getElementById('result')
-console.log(result.textContent)
-result.textContent = 12345
+const buttonCreate = document.getElementById('Create')
+
+// document.body.onload = addElement;
+
+
+
+
+
+buttonCreate.addEventListener('click', function () {
+
+    var my_div = document.querySelector('.grid-container');  
+    // let parent = document.querySelectorAll('.grid-container');
+    for (let i = 1; i <= 429; i++) {
+    const newDiv = document.createElement('div');
+    newDiv.classList.add('circle')
+    my_div.append(newDiv)
+
+    }
+
+    
+
+})
