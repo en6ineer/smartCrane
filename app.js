89f65632dc978ab5020a2b395467dfb90b4c5862
en6ineer <D.Kotenko@RailSystems.kz>
2024-02-12 16:00:55 +0600

    New func

--- app.js
+++ app.js
@@ -10,13 +10,21 @@ buttonCreate.addEventListener('click', function () {
 
     var my_div = document.querySelector('.grid-container');  
     // let parent = document.querySelectorAll('.grid-container');
-    for (let i = 1; i <= 429; i++) {
+    for (let i = 1; i <= 242; i++) { /* 429 */
     const newDiv = document.createElement('div');
     newDiv.classList.add('circle')
     my_div.append(newDiv)
 
     }
 
+    var my_div = document.querySelector('.grid-container2');  
+    // let parent = document.querySelectorAll('.grid-container');
+    for (let i = 1; i <= 186; i++) { /* 429 */
+    const newDiv = document.createElement('div');
+    newDiv.classList.add('circle')
+    my_div.append(newDiv)
+
+    }
     
 
 })
